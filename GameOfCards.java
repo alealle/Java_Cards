631386e28c08af2002979a0526e6113bd4041ad3
alealle <106605527+alealle@users.noreply.github.com>
2024-02-19 13:28:15 -0300

    Fix GameOfCards due to jdtls Mason bug. Now running correctly - fixed jdtls issue, installing Lsp server jdtls

--- GameOfCards.java
+++ GameOfCards.java
@@ -1,21 +1,19 @@
 public class GameOfCards {
-    private final int HAND_SIZE = 5;
-
-    
     public static void main(String[] args) {
-        Deck gameDeck = new Deck();
+
+        final int HAND_SIZE = 5;
         Card[] player1_hand = new Card[HAND_SIZE];
         Card[] player2_hand = new Card[HAND_SIZE];
-        int numRanks_1 = Deck.numRanks - 1; 
-        int numSuits_1 = Deck.numSuits - 1; 
+        int numRanks_1 = Deck.numRanks - 1;
+        int numSuits_1 = Deck.numSuits - 1;
 
-        for (int i =0; i < HAND_SIZE; i++){
-            int suit = 1 + Math.round(Math.random()*numSuits_1)));
-            int rank = 1 + Math.round(Math.random()*numRanks_1)));
+        for (int i = 0; i < HAND_SIZE; i++) {
+            int rank = 1 + Math.round((float) Math.random() * numRanks_1);
+            int suit = 1 + Math.round((float) Math.random() * numSuits_1);
             player1_hand[i] = new Card(rank, suit);
-         }
+        }
 
-        for (int i = 0; i < HAND_SIZE; i++){
+        for (int i = 0; i < HAND_SIZE; i++) {
             System.out.println(player1_hand[i]);
         }
     }
